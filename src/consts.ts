e40cdecd10b94385b5d6d54d44f3cf937a4c3e98
Matt Kane <m@mk.gg>
2025-05-06 14:43:12 +0100

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,64 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const populateBlocks = {
+  on: {
+    "blocks.hero": {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        links: true,
+      },
+    },
+    "blocks.heading-section": true,
+    "blocks.card-grid": {
+      populate: {
+        card: true,
+      },
+    },
+    "blocks.content-with-image": {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: true,
+      },
+    },
+    "blocks.faqs": {
+      populate: {
+        faq: true,
+      },
+    },
+    "blocks.person-card": {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+      },
+    },
+    "blocks.markdown": true,
+    "blocks.featured-articles": {
+      populate: {
+        articles: {
+          populate: {
+            featuredImage: {
+              fields: ["url", "alternativeText"],
+            },
+            author: {
+              populate: {
+                image: {
+                  fields: ["url", "alternativeText"],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "blocks.newsletter": true,
+  },
+};
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,20 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { strapiLoader } from "strapi-community-astro-loader";
+import { populateBlocks } from "./consts";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
