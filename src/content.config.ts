a4ed48fa6b0b7fd9576784cd0fd094c7414b02ab
Matt Kane <m@mk.gg>
2025-05-11 09:54:43 +0100

    Update

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,1 +1,8 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
+const blog = defineCollection({
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
+});
+
+export const collections = { blog };
